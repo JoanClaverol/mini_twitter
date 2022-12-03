2064d8f53c87e6f0235b15f15154959c1ddf5413
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2022-12-03 10:18:23 +0100

    result first meeting structure

---
+++ script.js
@@ -0,0 +1,30 @@
+let users_get = [
+    {
+        "id": "SbBGk",
+        " name": "....",
+    }, {
+        "id": "SbBGfez",
+        " name": "....",
+    }
+]
+
+const user_post = {
+    name: "...."
+}
+
+const messages_get = [
+    {
+        "id": "SbBGk",
+        "text": "....",
+    }, {
+        "id": "SbBGfez",
+        "text": "....",
+    }
+]
+
+const message_post = {
+    "id": "SbBGk",
+    "text": "....",
+}
+
+
